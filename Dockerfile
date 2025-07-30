aebc182c7fe227faef2f5119f286bbcab4aff6bc
Gaurav Kumar Singh <55310399+singhgaurav24@users.noreply.github.com>
2025-07-30 15:43:52 +0000

    inital

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # Dockerfile
 FROM python:3.11-slim
 WORKDIR /app
-COPY requirements.txt ./
+COPY app/requirements.txt ./
 RUN pip install -r requirements.txt
 COPY . .
 CMD ["python", "app.py"]
--- app/aap.py
+++ app/aap.py
@@ -1,16 +1,26 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
    return render_template('index.html')
+
 @app.route('/about')
 def about():
    return render_template('about.html')
+
 @app.route('/projects')
 def projects():
    return render_template('projects.html')
+
 @app.route('/contact')
 def contact():
    return render_template('contact.html')
+
+@app.route("/health")
+def health_check():
+    return "OK", 200
+
 if __name__ == '__main__':
-   app.run(debug=True)
+   app.run(host="0.0.0.0", port=5000, debug=True)
